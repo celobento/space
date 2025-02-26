b550446015901e05d6de89d8a238a059e21bffbc
Marcelo Bento <celobento26@gmail.com>
2025-02-26 15:53:50 -0300

    data base - orm - config

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+dados = {
+    1:  {"nome":"Nebulosa de Carina", "legenda":"webtelescope.oirb/NASA/JamesWebb"},
+    2:  {"nome":"Galaxia NGC 1079", "legenda":"bnasa.org/NASA/HUbble"}
+}
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
